239458e1774b3e4a824a81967a7437542e5fb222
Edoardo Ascenzi <edoardo.ascenzi97@gmail.com>
2024-10-23 11:18:41 +0200

    Update songs modules

--- React/app/src/modules.mjs
+++ React/app/src/modules.mjs
@@ -1,15 +1,16 @@
 
-export function Song(id, title, music, description = "", image = "./gandalf.gif") {
+export function Song(id, title, ext_url = "", length = "", description = "", image = ".assets/gandalf.gif") {
     this.id = id
     this.title = title
-    this.music = music
+    this.ext_url = ext_url
+    this.length = length
     this.description = description
     this.image = image
 }
 
 export const FAKE_SONGS = [
-    new Song(1, "Set 1", "", "Channel 1", ""),
-    new Song(2, "Set 2", "", "", ""),
+    new Song(1, "Set 1"),
+    new Song(2, "Set 2"),
     // new Song(3, "Setlkjn 2" ,"./What Happens In One Minute.mp3" ),
     // new Song(4, "Setnn 2" ,"./What Happens In One Minute.mp3" ),
     // new Song(5, "Setkjn 2" ,"./What Happens In One Minute.mp3" ),
