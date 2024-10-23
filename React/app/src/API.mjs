24cb6c3951f27b4ff53d873cc7181f97899e3cd3
Edoardo Ascenzi <edoardo.ascenzi97@gmail.com>
2024-10-23 11:19:34 +0200

    Add searchresult and audiostream (to fix) react API

--- React/app/src/API.mjs
+++ React/app/src/API.mjs
@@ -1,12 +1,33 @@
+import { Song } from './modules.mjs'
+
 const SERVER_URL = 'http://localhost:8000/';
+const SEARCH_ROUTE = 'search?q=';
+const PLAY_ROUTE = 'play?id=';
 
-async function getHello() {
-    const hello = await fetch(SERVER_URL)
-    .then(response => response.json())
-    return hello
-
+async function getAudioStream(source, id) {
+    const fullQuery = `${SERVER_URL}${source.toLowerCase()}${PLAY_ROUTE}${id}`;
+    const search = await fetch(fullQuery)
+        .then(response => response.json())
+        .catch(error => console.error('Error:', error));  // Handle any potential errors
+    return search;
 }
 
-const API = {getHello}
+async function getSearchResult(source, query) {
+    const fullQuery = `${SERVER_URL}${source.toLowerCase()}/${SEARCH_ROUTE}${encodeURIComponent(query)}`;
+    try {
+        const response = await fetch(fullQuery);
+        const res = await response.json();
+        // console.log(response)
+        const songs = res.results.map(result =>
+            new Song(result.video_id, result.title, result.video_url, result.length, result.channel, result.thumbnail_url)
+        )
+        console.log(songs)
+        return songs
+    } catch (error) {
+        console.error('Error fetching search results:', error);
+        return [];  // Return an empty array in case of error
+    }
+}
+const API = { getSearchResult }
 
 export default API
