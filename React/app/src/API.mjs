44ca709d77c4dd74bd0c63fd1a95150e0543334c
Edoardo Ascenzi <edoardo.ascenzi97@gmail.com>
2024-10-24 17:26:07 +0200

    Add getAudioStream react API

--- React/app/src/API.mjs
+++ React/app/src/API.mjs
@@ -1,33 +1,46 @@
 import { Song } from './modules.mjs'
 
 const SERVER_URL = 'http://localhost:8000/';
-const SEARCH_ROUTE = 'search?q=';
-const PLAY_ROUTE = 'play?id=';
+const SEARCH_ROUTE = '/search?q=';
+const PLAY_ROUTE = '/play?id=';
 
-async function getAudioStream(source, id) {
+
+async function getAudioStream(source, id) {//#, audioRef) {
     const fullQuery = `${SERVER_URL}${source.toLowerCase()}${PLAY_ROUTE}${id}`;
-    const search = await fetch(fullQuery)
-        .then(response => response.json())
-        .catch(error => console.error('Error:', error));  // Handle any potential errors
-    return search;
+    try {
+        const response = await fetch(fullQuery);
+
+        if (!response.ok) {
+            throw new Error(`Error fetching audio: ${response.statusText}`);
+        }
+
+        // Convert response to a Blob (binary large object)
+        const audioBlob = await response.blob();
+
+        // Create a URL for the blob to be used in the audio element
+        const audioUrl = URL.createObjectURL(audioBlob);
+
+        return audioUrl
+    } catch (error) {
+        console.error('Error fetching or playing audio:', error);
+        return ""
+    }
 }
 
+
 async function getSearchResult(source, query) {
-    const fullQuery = `${SERVER_URL}${source.toLowerCase()}/${SEARCH_ROUTE}${encodeURIComponent(query)}`;
+    const fullQuery = `${SERVER_URL}${source.toLowerCase()}${SEARCH_ROUTE}${encodeURIComponent(query)}`;
     try {
         const response = await fetch(fullQuery);
         const res = await response.json();
-        // console.log(response)
-        const songs = res.results.map(result =>
-            new Song(result.video_id, result.title, result.video_url, result.length, result.channel, result.thumbnail_url)
-        )
-        console.log(songs)
+        const songs = res.results.map(
+            result => new Song(result.video_id, result.title, source.toLowerCase(), result.video_url, result.length, result.channel, result.thumbnail_url))
         return songs
     } catch (error) {
         console.error('Error fetching search results:', error);
         return [];  // Return an empty array in case of error
     }
 }
-const API = { getSearchResult }
+const API = { getSearchResult, getAudioStream }
 
 export default API
