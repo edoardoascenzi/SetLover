e9ec334c498aeceda069e0df8b40d283b3baf2f9
Edoardo Ascenzi <edoardo.ascenzi97@gmail.com>
2024-10-24 17:26:29 +0200

    Update song modules to store the stream url

--- React/app/src/modules.mjs
+++ React/app/src/modules.mjs
@@ -1,16 +1,18 @@
 
-export function Song(id, title, ext_url = "", length = "", description = "", image = ".assets/gandalf.gif") {
+export function Song(id, title, source, ext_url = "", length = "", description = "", image = ".assets/gandalf.gif", stream_url = "") {
     this.id = id
     this.title = title
+    this.source = source
     this.ext_url = ext_url
     this.length = length
     this.description = description
     this.image = image
+    this.stream_url = stream_url
 }
 
 export const FAKE_SONGS = [
-    new Song(1, "Set 1"),
-    new Song(2, "Set 2"),
+    new Song(1, "Set 1", "youtube"),
+    new Song(2, "Set 2", "soundcloud"),
     // new Song(3, "Setlkjn 2" ,"./What Happens In One Minute.mp3" ),
     // new Song(4, "Setnn 2" ,"./What Happens In One Minute.mp3" ),
     // new Song(5, "Setkjn 2" ,"./What Happens In One Minute.mp3" ),
