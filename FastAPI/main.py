2bfc9406bdcc8ed5f900f2d6aeb74dd65b2da850
Edoardo Ascenzi <edoardo.ascenzi97@gmail.com>
2024-10-25 10:49:50 +0200

    Add lifespan (startup and shutdown execution) and cleanup schedule job
    fix main

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,9 +1,39 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from .routers import youtube
 from fastapi.middleware.cors import CORSMiddleware
+import logging
+import os
 
-app = FastAPI()
+from FastAPI.utils.const import *
+from .routers import youtube
+from FastAPI.utils.utils import *
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    ## START STARTUP
+    if not os.path.exists(TEMP_AUDIO_DIR):
+        # Create the temp audio dir
+        os.makedirs(TEMP_AUDIO_DIR)
+    #run the clean temp file in the startup 
+    cleanup_temp_files()
+    # add the clean temp file job to a schedule
+    scheduler.add_job(cleanup_temp_files, 'interval', minutes=TIMEOUT_DELETE_TEMP_AUDIO_H)
+    scheduler.start()
+    ## END STARTUP
+    yield
+    ## START SHUTDOWN
+    scheduler.shutdown()
+    ## END SHUTDOWN
+
+# Scheduler instance
+scheduler = BackgroundScheduler()
+
+app = FastAPI(lifespan=lifespan)
 
 origins = [
     "http://localhost:5173",
@@ -18,8 +48,3 @@ app.add_middleware(
 )
 
 app.include_router(youtube.router)
-
-
-@app.get("/")
-async def root():
-    return {"message":"hello world"}
