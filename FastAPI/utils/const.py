faae40a8bf64c63c94d1fb17abf84be38a3aec2b
Edoardo Ascenzi <edoardo.ascenzi97@gmail.com>
2024-10-25 10:48:51 +0200

    Add const file

---
+++ FastAPI/utils/const.py
@@ -0,0 +1,10 @@
+# Directory for temporary audio files
+TEMP_AUDIO_DIR = "audio_temp"
+
+# Temp audios accessed before this hours are be deleted
+TIMEOUT_DELETE_TEMP_AUDIO_H = 1 #6
+
+# Interval in minutes of the cleanup job
+CLEANUP_JOB_INTERVAL_M = 1 #10
+
+
